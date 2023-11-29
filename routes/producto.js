9710a11f025858a58ac3ccd6df1c5613da8316e2
Daniel Alejandro Mesa Lopez <danielmesalopez@hotmail.com>
2023-11-28 19:41:36 -0500

    actualizacion de metodos

--- routes/producto.js
+++ routes/producto.js
@@ -8,7 +8,7 @@ const {connection} = require("../config.db");
 
 //Utilizando el método Get 
 const getProductos = (request, response) => {
-    connection.query("SELECT * FROM productos", 
+    connection.query("SELECT * FROM productos where estado=1", 
     (error, results) => {
         if(error)
             throw error;
@@ -27,16 +27,45 @@ const postProductos = (request, response) => {
     [id_productos, id_categoria, descripcion_producto, image, precio, cantidad, estado],
     (error, results) => {
         if (error) throw error;
-        response.status(201).json({"Item creado correctamente": results.affectedRows});
+        response.status(201).json({"Producto creado correctamente": results.affectedRows});
     });
 };
  
 //ruta
-app.route("/productocrear")
+app.route("/productoCrear")
 .post(postProductos);
 
 
-//Actualizar producto
+// Actualizar producto
+const putProductos = (request, response) => {
+    const {id_productos, id_categoria, descripcion_producto, image, precio, cantidad, estado} = request.body;
+    connection.query("UPDATE productos set id_categoria=?, descripcion_producto=?, image=?, precio=?, cantidad=?, estado= ?  where id_productos=?",
+    [id_categoria, descripcion_producto, image, precio, cantidad, estado, id_productos],
+    (error, results) => {
+       if(error)
+          throw error;
+       response.status(201).json({"Producto actualizado correctamente": results.affectedRows});
+      });
+    };
+   
+    //ruta
+    app.route("/productoA")
+    .put(putProductos);
 
+    //Eliminar Producto
+const delProductos= (request, response) => {
+    const id_productos = request.params.id_productos;
+    connection.query("update  productos set estado=0 where id_productos = ?",
+    [id_productos],
+    (error, results) => {
+        if(error)
+            throw error;
+        response.status(201).json({"Producto eliminado":results.affectedRows});
+    });
+};
+ 
+//ruta
+app.route("/productos/:id_productos")
+.delete(delProductos);
 
 module.exports = app;
